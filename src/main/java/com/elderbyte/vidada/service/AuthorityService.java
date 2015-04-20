c64adf9f596593480256661443ad415a69378346
IsNull <isnull@securityvision.ch>
2015-04-20 16:47:38 +0200

    introduced auth service

---
+++ src/main/java/com/elderbyte/vidada/service/AuthorityService.java
@@ -0,0 +1,53 @@
+package com.elderbyte.vidada.service;
+
+import com.elderbyte.vidada.domain.security.Authority;
+import com.elderbyte.vidada.domain.security.KnownAuthority;
+import com.elderbyte.vidada.repository.AuthorityRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+
+/**
+ * Manages security authorities (roles)
+ */
+@Service
+public class AuthorityService {
+
+
+    private final AuthorityRepository authorityRepository;
+
+    @Inject
+    public AuthorityService(AuthorityRepository authorityRepository){
+        this.authorityRepository = authorityRepository;
+        ensureDefaultAuthorities();
+    }
+
+
+    public Authority get(String name){
+        return authorityRepository.findOne(name);
+    }
+
+    @Transactional
+    public Authority create(String authorityName){
+        Authority auth = new Authority(authorityName);
+        authorityRepository.save(auth);
+        return auth;
+    }
+
+
+    /**
+     * Ensures that all known authorities exist in the database
+     */
+    @Transactional
+    private void ensureDefaultAuthorities(){
+        for(String role : KnownAuthority.values()) {
+            Authority auth = get(role);
+            if (auth == null) {
+                create(role);
+            }
+        }
+    }
+
+
+}
