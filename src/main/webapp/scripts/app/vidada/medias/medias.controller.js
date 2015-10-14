50da197c67f44050710652cd18db4c1b85afef89
IsNull <isnull@securityvision.ch>
2015-10-14 15:43:33 +0200

    working media search and infinite scroll

--- src/main/webapp/scripts/app/vidada/medias/medias.controller.js
+++ src/main/webapp/scripts/app/vidada/medias/medias.controller.js
@@ -8,7 +8,6 @@ angular.module('vidadaApp')
         $scope.someText = "huhu";
         $scope.mediaService = new MediaInfinite("");
 
-
         $scope.$watch("query", function(newValue, oldValue) {
             $scope.mediaService = new MediaInfinite(newValue);
             $scope.someText = "Updated media " + newValue;
@@ -17,6 +16,27 @@ angular.module('vidadaApp')
 
         $scope.escape = function(text) {
             return escape(text);
-        }
+        };
+
+        // Handles scroll events
+        var scrollHandler = function() {
+            var isOnBottom = $(window).scrollTop() == ($(document).height() - $(window).height());
+            if(isOnBottom){
+                // We reached the bottom of the page ...
+                $scope.$apply(function(){
+                    // .. time to load more data!
+                    $scope.mediaService.nextPage();
+                });
+            }
+        };
+
+        $(function(){
+            $(document).scroll(scrollHandler);
+        });
+
+        $scope.$on('$destroy', function() {
+            // Clean up resources here
+            $(document).off("scroll", scrollHandler); // Remove the jquery scroll handler
+        });
 
     });
--- src/main/webapp/scripts/components/vidada/media.service.js
+++ src/main/webapp/scripts/components/vidada/media.service.js
@@ -46,23 +46,31 @@ angular.module('vidadaApp')
             if (this.busy) return;
             this.busy = true;
 
-            if(this.itemCount <=  this.lastLoadedPage * this.itemsPerPage){
+            var alreadyLoaded = this.lastLoadedPage+1 * this.itemsPerPage;
+
+            if(this.itemCount >=  alreadyLoaded){
                 // Only load new data if we don't have all!
                 var pageToLoad = this.lastLoadedPage + 1;
 
-                MediaPage.load(pageToLoad, 5, this.query).$promise.then(function (data) {
+                console.log("Requesting page " + pageToLoad + "...");
+
+                MediaPage.load(pageToLoad, 15, this.query).$promise.then(function (data) {
 
                     this.itemCount = data.totalListSize;
                     this.itemsPerPage = data.maxPageSize;
                     this.lastLoadedPage = data.page;
 
                     var newItems = data.pageItems;
+
+                    console.log("Got page " + data.page + " with " + newItems.length + " items!");
+
                     for (var i = 0; i < newItems.length; i++) {
                         this.items.push(newItems[i]);
                     }
                     this.busy = false;
                 }.bind(this));
             }else{
+                console.log("Already all pages loaded, skipping nextPage() request!");
                 this.busy = false;
             }
         };
