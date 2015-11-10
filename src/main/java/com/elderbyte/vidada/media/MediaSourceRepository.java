65feab6dc79bdb2f3f171bddc551c5842538625a
IsNull <isnull@securityvision.ch>
2015-11-10 09:54:52 +0100

    added Repository anotations

--- src/main/java/com/elderbyte/vidada/media/MediaSourceRepository.java
+++ src/main/java/com/elderbyte/vidada/media/MediaSourceRepository.java
@@ -2,11 +2,13 @@ package com.elderbyte.vidada.media;
 
 import com.elderbyte.vidada.media.source.MediaSource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 /**
  * Spring Data JPA repository for the User entity.
  */
+@Repository
 public interface MediaSourceRepository extends JpaRepository<MediaSource, Integer> {
 
 
--- src/main/java/com/elderbyte/vidada/media/libraries/MediaLibraryRepository.java
+++ src/main/java/com/elderbyte/vidada/media/libraries/MediaLibraryRepository.java
@@ -2,10 +2,12 @@ package com.elderbyte.vidada.media.libraries;
 
 import com.elderbyte.vidada.media.libraries.MediaLibrary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data JPA repository for the MediaLibrary entity.
  */
+@Repository
 public interface MediaLibraryRepository extends JpaRepository<MediaLibrary, Integer> {
 
 
