66836c9c0214bb0fdc28e1c3bedce7e9b7db19a0
IsNull <isnull@securityvision.ch>
2015-12-10 09:10:08 +0100

    improved JWT auth provider, handle expiration dates correctly

--- src/main/java/com/elderbyte/server/security/jwt/JwtAuthenticationException.java
+++ src/main/java/com/elderbyte/server/security/jwt/JwtAuthenticationException.java
@@ -8,8 +8,13 @@ import org.springframework.security.core.AuthenticationException;
 @SuppressWarnings("serial")
 public class JwtAuthenticationException extends AuthenticationException {
 
+
     public JwtAuthenticationException(String msg) {
         super(msg);
+    }
+
+    public JwtAuthenticationException(String msg, Throwable cause) {
+        super(msg, cause);
     }
 
 }
--- src/main/java/com/elderbyte/server/security/jwt/JwtAuthenticationProvider.java
+++ src/main/java/com/elderbyte/server/security/jwt/JwtAuthenticationProvider.java
@@ -10,6 +10,11 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.MACVerifier;
 
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 @Component
 public class JwtAuthenticationProvider implements AuthenticationProvider {
 
@@ -26,20 +31,43 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
             JWSVerifier verifier = new MACVerifier(SecretKey);
             boolean isVerified = jwtToken.getSignedToken().verify(verifier);
             if (isVerified) {
-                //logger.info("JWT is verified and authenticated!");
+
+                // The token is valid -  check if it is activated and not expired
+                Date expirationDate = jwtToken.getSignedToken().getJWTClaimsSet().getExpirationTime();
+                if(expirationDate != null){
+                    if(LocalDateTime.now().isAfter(toDateTime(expirationDate))){
+                        // The token has expired!
+                        throw new JwtAuthenticationException("Authentication failed - The provided token is valid but already expired!");
+                    }
+                }
+
+                Date notValidBefore = jwtToken.getSignedToken().getJWTClaimsSet().getNotBeforeTime();
+                if(notValidBefore != null){
+                    if(LocalDateTime.now().isBefore(toDateTime(notValidBefore))){
+                        // The token is not yet valid!
+                        throw new JwtAuthenticationException("Authentication failed - The provided token is valid not yet valid (notValidBefore is set to " + toDateTime(notValidBefore).toString() + "!");
+                    }
+                }
+
                 jwtToken.setAuthenticated(true);
             } else {
-                throw new JwtAuthenticationException("Authentication failed - MAC not matching!");
+                throw new JwtAuthenticationException("Authentication failed - MAC signature not matching!");
             }
             return jwtToken;
         } catch (JOSEException e) {
-            throw new JwtAuthenticationException("authentication failed");
+            throw new JwtAuthenticationException("Authentication failed", e);
+        } catch (ParseException e) {
+            throw new JwtAuthenticationException("Authentication failed - Could not read expiration header!", e);
         }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
         return true;
+    }
+
+    private static LocalDateTime toDateTime(Date date){
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
 }
