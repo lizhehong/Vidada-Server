e88a9249f331a063d772411e5b2e0779388ffd31
IsNull <isnull@ElderBook.elder-router>
2015-12-30 15:22:20 +0100

    improved media player, support images

--- src/main/resources/static/scripts/app/vidada/player/player.controller.js
+++ src/main/resources/static/scripts/app/vidada/player/player.controller.js
@@ -2,5 +2,14 @@
 
 angular.module('vidadaApp')
     .controller('MediaPlayerController', function ($scope, $stateParams, Media, media) {
+
+        var type =  media.mediaType.toLowerCase();
+
+        console.log("Playing media type " + type);
+
+        media.isVideo = (type == 'movie');
+        media.isImage = (type == 'image');
+        media.isSound = (type == 'sound');
+
         $scope.media = media;
     });
