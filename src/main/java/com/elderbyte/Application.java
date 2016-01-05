51e92ba94d53b40c9bf17bbcdacd58a6446fbfa3
IsNull <isnull@ElderBook.elder-router>
2016-01-05 12:51:22 +0100

    clean up

--- src/main/java/com/elderbyte/Application.java
+++ src/main/java/com/elderbyte/Application.java
@@ -5,14 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
