32d0048cfafe9492c758e678d262553f026303a2
IsNull <isnull@ElderBook.elder-router>
2016-02-24 20:39:52 +0100

    gulpfile improved

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var uglify = require('gulp-uglify');
 var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
+var ngmin = require('gulp-ngmin');
 
 
 var bases = {
@@ -19,12 +20,7 @@ var bases = {
     dist: 'src/main/resources/static/dist/'
 };
 
-var paths = {
-    //styles: [bases.app + 'assets/styles/**/*.css'],
-    html: [bases.app + 'scripts/**/*.html'],
-    images: [bases.app + 'assets/images/**/*.png', 'images/**/*.png'],
-    extras: [bases + 'robots.txt', 'favicon.ico']
-};
+
 
 
 // === TASKS ===
@@ -41,7 +37,7 @@ gulp.task('usemin', function() {
         .pipe(usemin({
             css: [ rev ],
             html: [ function () {return minifyHtml({ empty: true });} ],
-            js: [ uglify, rev ],
+            js: [ ngmin, uglify({ mangle: false }), rev ],
             inlinejs: [ uglify ],
             inlinecss: [ minifyCss, 'concat' ]
         }))
@@ -53,13 +49,54 @@ gulp.task('usemin', function() {
 // Copy all other files to dist directly
 gulp.task('copy', ['clean'], function() {
     // Copy html
-    gulp.src(paths.html)
-        .pipe(gulp.dest(bases.dist));
+    gulp.src(bases.app + 'scripts/**/*.html')
+        .pipe(gulp.dest(bases.dist + 'scripts'));
+
+    // Copy images
+    gulp.src(bases.app + 'assets/images/**/*.png')
+        .pipe(gulp.dest(bases.dist+'assets/images/'));
+
+    // Copy i18n
+    gulp.src(bases.app + 'i18n/**/*.json')
+        .pipe(gulp.dest(bases.dist+'i18n/'));
 
     // Copy extra html5bp files
-    gulp.src(paths.extras)
+    gulp.src([bases.app + 'robots.txt', bases.app + 'favicon.ico', bases.app + '*.json'])
         .pipe(gulp.dest(bases.dist));
 });
+
+
+/**
+ * Wire-up the bower dependencies
+ * @return {Stream}
+
+gulp.task('wiredep', function() {
+    log('Wiring the bower dependencies into the html');
+
+    var wiredep = require('wiredep').stream;
+    var options = config.getWiredepDefaultOptions();
+
+    // Only include stubs if flag is enabled
+    var js = args.stubs ? [].concat(config.js, config.stubsjs) : config.js;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe(inject(js, '', config.jsOrder))
+        .pipe(gulp.dest(config.client));
+});
+ */
+/*
+gulp.task('inject', ['wiredep', 'styles', 'templatecache'], function() {
+    log('Wire up css into the html, after files are ready');
+
+    return gulp
+        .src(config.index)
+        .pipe(inject(config.css))
+        .pipe(gulp.dest(config.client));
+});
+*/
+
 
 // A development task to run anytime a file changes
 gulp.task('watch', function() {
